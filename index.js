1071531c08cb4dcf81a29508e29988945f1724e7
Rohan Shrivastava <rohanshrivastava10000@gmail.com>
2022-12-25 02:34:22 +0530

    Added basic routes, controllers, views

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// use express router using middleware
+app.use('/',require('./routes/index.js'));
+
+// use ejs as the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port, function (error) {
   if (error) {
-    console.log(`Error in running the server: $(error)`);
+    console.log(`Error in running the server: ${error}`);
   } else {
-    console.log(`Server is running on port: $(port)`);
+    console.log(`Server is running on port: ${port}`);
   }
 });
